595b9374470bae254e412f4eff4a2cc029c77646
Stephen Akoto <akotosteven@gmail.com>
2023-03-16 12:39:04 -0500

    Build an Icon List

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/client-api";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGameResponse {
